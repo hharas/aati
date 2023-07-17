7d0fccaeec912df923fac069cb554a4d7651016e
hharas <husayn@dnmx.org>
2023-07-17 16:57:48 +0300

    Migrating to clap-rs

--- src/main.rs
+++ src/main.rs
@@ -16,51 +16,23 @@
    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+#![allow(unused)]
+
+use clap::{Arg, ArgAction, Command};
+use version::get_version;
+
 mod commands;
 mod globals;
 mod types;
 mod utils;
 mod version;
 
-use colored::Colorize;
-use std::{env, process::exit};
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() > 1 {
-        match args.get(1).map(String::as_str) {
-            Some("--help") => {
-                println!(
-                    "aati - Cross-platform package manager written in Rust
-
-Usage: aati [COMMANDS/OPTIONS]
-
-Commands:
-    get <package>                    Download a package from the Repository and install it
-    install <path/to/archive>        Install a package from an LZ4 Archive
-    upgrade [package]                Upgrade a package or all packages (alias: update)
-    remove <package>/<-all>[--force] Remove a package (alias: uninstall)
-    list [installed/available]       List installed or available packages
-    sync                             Update package index
-    repo                             Package Repository Management
-      add <url://to/repo>              Add a package repository
-      remove <repo name>               Remove a package repository
-      list                             List all added package repositories
-      info <repo name>                 Show an overview of a repository
-      init                             Initialise a new package repository
-    info <package>                   Show a package's info
-    changelog [package] [--latest]   Show Aati's or a package's changelog
-    package <path/to/binary>         Compress a binary into LZ4
-    generate                         Generate .html files for a package repository
-    serve [host:port]                Host a package web index (default: localhost:8887)
-
-Options:
-    -V, --version Print version info
-        --about   Information about Aati
-        --help    Show this help message
-
-Copyright (C) 2023  Husayn Haras
+    let name: &str = "aati";
+    let version = get_version();
+    let author: &str = "Husayn Haras <husayn@dnmx.org>";
+    let about: &str = "Cross-platform package manger written in Rust";
+    let after_help: &str = "Copyright (C) 2023  Husayn Haras
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of version 3 of the GNU General Public License
@@ -72,155 +44,198 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 User Guide: https://github.com/hharas/aati/wiki/2.-User-Guide
-Issue tracker: https://github.com/hharas/aati/issues"
-                );
-            }
+Issue tracker: https://github.com/hharas/aati/issues";
 
-            Some("--about") => {
-                println!(
-                    "Aati is a Cross-platform Package Manager written in Rust. The Aati Package Manager focuses on providing a Simple,
-Efficient and Performant interface for installing, managing and removing packages. Aati is not made (but can be forked)
-to be a system-wide package manager like Pacman or APT but rather a user-specific package manager. Aati supports
-multiple operating systems including Linux, Windows, Android (on Termux) and more.
-
-Aati also has its own Packaging System revolving around PKGFILEs, that are files describing the installation and removal
-process of packages in a custom TOML-like language, which is a concept somewhat similar to the Arch Build System's
-PKGBUILDs. The Aati Package Manager recognises files ending with `.tar.lz4` as package distributions that can be
-installed, either from an online package repository or from the local filesystem.
-
-See the Wiki @ https://github.com/hharas/aati/wiki/2.-User-Guide on how to start using Aati."
-                );
-            }
-
-            Some("-V") | Some("--version") => {
-                let aati_version = version::get_version();
-
-                println!("aati version {}", aati_version,);
-            }
-
-            Some("get") => match args.get(2) {
-                Some(_) => {
-                    commands::get(&args[2..]);
-                }
-
-                None => {
-                    println!("{}", "- No package name?".bright_red());
-                    exit(1);
-                }
-            },
-
-            Some("upgrade") | Some("update") => commands::upgrade(&args[2..]),
-
-            Some("uninstall") | Some("remove") => match args.get(2) {
-                Some(_) => {
-                    commands::remove(&args[2..]);
-                }
-
-                None => {
-                    println!("{}", "- No package name?".bright_red());
-                    exit(1);
-                }
-            },
-
-            Some("list") => {
-                if let Some(arg) = args.get(2) {
-                    let choice = arg;
-                    commands::list(Some(choice));
-                } else {
-                    commands::list(None);
-                }
-            }
-
-            Some("sync") => commands::sync(),
-
-            Some("repo") => {
-                if let Some(arg1) = args.get(2) {
-                    if let Some(arg2) = args.get(3) {
-                        commands::repo(Some(arg1), Some(arg2));
-                    } else {
-                        commands::repo(Some(arg1), None)
-                    }
-                } else {
-                    commands::repo(None, None);
-                }
-            }
-
-            Some("info") => match args.get(2) {
-                Some(package_name) => {
-                    commands::info(package_name, None);
-                }
-
-                None => {
-                    println!("{}", "- No package name?".bright_red());
-                    exit(1);
-                }
-            },
-
-            Some("changelog") => match args.get(2) {
-                Some(arg1) => match args.get(3) {
-                    Some(arg2) => {
-                        if arg1 == "--latest" {
-                            commands::changelog(Some(arg2), true)
-                        } else if arg2 == "--latest" {
-                            commands::changelog(Some(arg1), true)
-                        } else {
-                            println!("{}", "- One package at a time!".bright_red());
-                            exit(1);
-                        }
-                    }
-                    None => {
-                        if arg1 == "--latest" {
-                            commands::changelog(None, true)
-                        } else {
-                            commands::changelog(Some(arg1), false)
-                        }
-                    }
-                },
-
-                None => commands::changelog(None, false),
-            },
-
-            Some("package") => match args.get(2) {
-                Some(package_directory) => {
-                    commands::package(package_directory.to_string());
-                }
-
-                None => {
-                    println!("{}", "- No package name?".bright_red());
-                    exit(1);
-                }
-            },
-
-            Some("install") => match args.get(2) {
-                Some(package_name) => {
-                    commands::install(package_name);
-                }
-
-                None => {
-                    println!("{}", "- No archive name?".bright_red());
-                    exit(1);
-                }
-            },
-
-            Some("generate") => commands::generate(),
-
-            Some("serve") => match args.get(2) {
-                Some(address) => {
-                    commands::serve(Some(address));
-                }
-
-                None => {
-                    commands::serve(None);
-                }
-            },
-
-            _ => {
-                println!("{}", "- Unknown command!".bright_red())
-            }
-        }
-    } else {
-        println!("Try 'aati --help' for more information");
-    }
+    let matches = Command::new(name)
+        .author(author)
+        .version(version)
+        .about(about)
+        .after_help(after_help)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommands([
+            Command::new("get")
+                .short_flag('G')
+                .about("Download and install packages from an available repository")
+                .arg(
+                    Arg::new("packages")
+                        .help("package/s to get")
+                        .action(ArgAction::Set)
+                        .required(true)
+                        .num_args(1..),
+                ),
+            Command::new("install")
+                .short_flag('I')
+                .about("Install a package from a local .tar.lz4 archive")
+                .arg(
+                    Arg::new("package")
+                        .help("package .tar.lz4 file")
+                        .action(ArgAction::Set)
+                        .required(true)
+                        .num_args(1),
+                ),
+            Command::new("upgrade")
+                .visible_alias("update")
+                .short_flag('U')
+                .about("Upgrade local packages to their latest versions")
+                .arg(
+                    Arg::new("packages")
+                        .help("package/s to upgrade")
+                        .action(ArgAction::Set)
+                        .num_args(1..),
+                ),
+            Command::new("remove")
+                .visible_alias("uninstall")
+                .short_flag('R')
+                .about("Remove an installed package")
+                .args([
+                    Arg::new("packages")
+                        .required(true)
+                        .action(ArgAction::Set)
+                        .help("package/s to remove")
+                        .num_args(1..),
+                    Arg::new("all")
+                        .long("all")
+                        .short('a')
+                        .action(ArgAction::SetTrue)
+                        .conflicts_with("packages")
+                        .help("Remove all packages"),
+                    Arg::new("force")
+                        .long("force")
+                        .short('f')
+                        .action(ArgAction::SetTrue)
+                        .help("Apply forceful removal"),
+                ]),
+            Command::new("list")
+                .short_flag('L')
+                .about("List installed packages")
+                .arg(
+                    Arg::new("available")
+                        .long("available")
+                        .short('a')
+                        .help("list available packages")
+                        .action(ArgAction::SetTrue),
+                ),
+            Command::new("sync")
+                .short_flag('S')
+                .about("Sync repository manifests"),
+            Command::new("repo")
+                .short_flag('P')
+                .about("Manage repositories")
+                .subcommands([
+                    Command::new("add")
+                        .short_flag('a')
+                        .about("Add a repository")
+                        .arg(
+                            Arg::new("url")
+                                .help("Repository URL")
+                                .action(ArgAction::Set)
+                                .required(true)
+                                .num_args(1),
+                        ),
+                    Command::new("remove")
+                        .short_flag('r')
+                        .about("Remove a repository")
+                        .arg(
+                            Arg::new("name")
+                                .help("Repository name")
+                                .action(ArgAction::Set)
+                                .required(true)
+                                .num_args(1),
+                        ),
+                    Command::new("info")
+                        .short_flag('i')
+                        .about("Show a repository's metadata")
+                        .arg(
+                            Arg::new("name")
+                                .help("Repository name")
+                                .action(ArgAction::Set)
+                                .required(true)
+                                .num_args(1),
+                        ),
+                ])
+                .args([
+                    Arg::new("list")
+                        .short('l')
+                        .long("list")
+                        .help("List added repositories")
+                        .conflicts_with("init")
+                        .action(ArgAction::SetTrue),
+                    Arg::new("init")
+                        .short('n')
+                        .long("init")
+                        .conflicts_with("list")
+                        .help("Initialise a new repository")
+                        .action(ArgAction::SetTrue),
+                ]),
+            Command::new("query")
+                .short_flag('Q')
+                .about("Query a package's metadata")
+                .arg(
+                    Arg::new("package")
+                        .help("selected package to query")
+                        .required(true)
+                        .action(ArgAction::Set)
+                        .num_args(1),
+                ),
+            Command::new("changelog")
+                .short_flag('C')
+                .about("Display a package's changelog")
+                .args([
+                    Arg::new("package")
+                        .help("selected package")
+                        .action(ArgAction::Set)
+                        .num_args(1),
+                    Arg::new("latest")
+                        .short('l')
+                        .long("latest")
+                        .help("show only the latest changes")
+                        .action(ArgAction::SetTrue),
+                ]),
+            Command::new("package")
+                .short_flag('K')
+                .about("Compress a directory into a .tar.lz4 package archive")
+                .arg(
+                    Arg::new("directory")
+                        .help("path to package directory")
+                        .action(ArgAction::Set)
+                        .required(true)
+                        .num_args(1),
+                ),
+            Command::new("generate")
+                .short_flag('N')
+                .about("Generate HTML files for a repository"),
+            Command::new("serve")
+                .short_flag('E')
+                .about("Serve a package web index using a repo.toml")
+                .args([
+                    Arg::new("host")
+                        .long("host")
+                        .short('h')
+                        .required(true)
+                        .action(ArgAction::Set)
+                        .help("server host"),
+                    Arg::new("port")
+                        .long("port")
+                        .short('p')
+                        .default_value("8887")
+                        .action(ArgAction::Set)
+                        .help("server port"),
+                    Arg::new("repository")
+                        .long("repo")
+                        .short('r')
+                        .required(true)
+                        .action(ArgAction::Set)
+                        .help("repository url"),
+                    Arg::new("url")
+                        .long("url")
+                        .short('u')
+                        .required(true)
+                        .action(ArgAction::Set)
+                        .help("server url (e.g. http://example.com)"),
+                ]),
+        ])
+        .get_matches();
 }
 
 // تم بحمد الله
